21ad544025519030dfc6d4ab5984eaab9031c38d
Marvin Hübner <mjh@bergwerk.ag>
2015-09-30 09:21:23 +0200

    [task] demo files

--- demo/demo.js
+++ demo/demo.js
@@ -3,7 +3,29 @@ var sliderInit = $('.slick-slider-init');
 $(document).ready(function () {
 
     sliderInit.slick({
-        autoplay: false
+        autoplay: true,
+        autoplaySpeed: 9000
     });
     sliderInit.slickAnimation();
 });
+
+
+//function slickAddDelayIn(object) {
+//    var delayInAttr = [
+//        'animation-delay',
+//        '-webkit-animation-delay',
+//        '-moz-animation-delay',
+//        '-o-animation-delay',
+//        '-ms-animation-delay'
+//    ];
+//
+//    $.each(delayInAttr, function(){
+//        var attr = $(this);
+//        object.css({attr : delayIn + 's'});
+//    });
+//
+//    delayInAttr.forEach(function(delayTime){
+//        object.css({delayTime: delayIn + 's'});
+//    });
+//}
+
