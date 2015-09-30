64ad026ae047b53c046368cf981144a7f6058537
Marvin Hübner <mjh@bergwerk.ag>
2015-09-30 16:38:48 +0200

    [task] commit

--- demo/demo.js
+++ demo/demo.js
@@ -3,29 +3,12 @@ var sliderInit = $('.slick-slider-init');
 $(document).ready(function () {
 
     sliderInit.slick({
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 9000
     });
+
     sliderInit.slickAnimation();
 });
 
 
-//function slickAddDelayIn(object) {
-//    var delayInAttr = [
-//        'animation-delay',
-//        '-webkit-animation-delay',
-//        '-moz-animation-delay',
-//        '-o-animation-delay',
-//        '-ms-animation-delay'
-//    ];
-//
-//    $.each(delayInAttr, function(){
-//        var attr = $(this);
-//        object.css({attr : delayIn + 's'});
-//    });
-//
-//    delayInAttr.forEach(function(delayTime){
-//        object.css({delayTime: delayIn + 's'});
-//    });
-//}
 
